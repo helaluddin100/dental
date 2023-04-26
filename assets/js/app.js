99864eb6c51eef1c61dc555456b4359f23662a28
Helal Uddin <freelancerhelaluddin@gmail.com>
2023-04-26 21:42:41 +0600

    service all page design done

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+ var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: ".testimonial-next",
+          prevEl: ".testimonial-prev",
+        },
+      });
