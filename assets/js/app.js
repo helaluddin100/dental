178076471b266436f0ad6320920eca3427906a60
Helal Uddin <freelancerhelaluddin@gmail.com>
2023-04-28 09:13:19 +0600

    header link and service link

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,27 @@
- var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        navigation: {
-          nextEl: ".testimonial-next",
-          prevEl: ".testimonial-prev",
-        },
-      });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".testimonial-next",
+    prevEl: ".testimonial-prev",
+  },
+});
+
+// FOR RESUME CATEGORY ACTIVE BUTTON
+$(".service-show-link").on("click", function () {
+  $(".service-show-link").removeClass("active");
+  $(this).addClass("active");
+});
+
+// FOR RESUME CATEGORY ACTIVE CONTENT
+function filter(active) {
+  hide = document.getElementsByClassName("ss_item");
+  for (i = 0; i < hide.length; i++) {
+    hide[i].style.display = "none";
+  }
+
+  let show = document.getElementsByClassName(active);
+  for (i = 0; i < hide.length; i++) {
+    show[i].style.display = "flex";
+  }
+}
