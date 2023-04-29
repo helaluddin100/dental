771d597ffe216d44296bdba1ebc4e462c746798f
Helal Uddin <freelancerhelaluddin@gmail.com>
2023-04-29 16:43:32 +0600

    tooth extention page issue fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,3 +30,40 @@ function filter(active) {
     show[i].style.display = "flex";
   }
 }
+
+// FOR RESUME CATEGORY ACTIVE CONTENT
+function filter(active) {
+  hide = document.getElementsByClassName("location-area");
+  for (i = 0; i < hide.length; i++) {
+    hide[i].style.display = "none";
+  }
+
+  let show = document.getElementsByClassName(active);
+  for (i = 0; i < hide.length; i++) {
+    show[i].style.display = "block";
+  }
+}
+// FOR RESUME CATEGORY ACTIVE BUTTON
+$(".location-tab").on("click", function () {
+  $(".location-tab").removeClass("active");
+  $(this).addClass("active");
+});
+
+// ===============================faq============
+// FOR FAQ SECTION ACCORDION
+$(function ($) {
+  var panels = $(".faq__ans").hide();
+  panels.first().show();
+
+  $(".faq__que").click(function () {
+    var $this = $(this);
+    panels.slideUp();
+    $this.next().slideDown();
+  });
+});
+
+$(".faq__que").click(function () {
+  $(".faq__que i").addClass("icofont-plus");
+  $(this.children[1]).removeClass("icofont-plus");
+  $(this.children[1]).addClass("icofont-minus");
+});
